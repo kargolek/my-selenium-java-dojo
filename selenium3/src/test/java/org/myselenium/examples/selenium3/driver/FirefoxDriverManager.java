2a7ad8df2942c10fb347f197243de2bc5ea2d2d8
karol_orlowicz <karol.orlowicz@gmail.com>
2021-08-16 22:20:00 +0200

    init origin branch

---
+++ selenium3/src/test/java/org/myselenium/examples/selenium3/driver/FirefoxDriverManager.java
@@ -0,0 +1,9 @@
+package org.myselenium.examples.selenium3.driver;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class FirefoxDriverManager {
+
+    final FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+}
