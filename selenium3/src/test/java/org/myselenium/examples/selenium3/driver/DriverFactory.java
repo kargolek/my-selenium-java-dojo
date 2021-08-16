98a1b7a14af2d8ec98630c4554ed04649b30a11d
karol_orlowicz <karol.orlowicz@gmail.com>
2021-08-16 02:43:20 +0200

    fix

--- selenium3/src/test/java/org/myselenium/examples/selenium3/driver/DriverFactory.java
+++ selenium3/src/test/java/org/myselenium/examples/selenium3/driver/DriverFactory.java
@@ -24,6 +24,6 @@ public class DriverFactory {
     }
 
     public static WebDriver getDriver() throws MalformedURLException {
-        return getDriver(DriverType.REMOTE);
+        return System.getenv("CI").equals("true") ? getDriver(DriverType.REMOTE) : getDriver(DriverType.CHROME);
     }
 }
