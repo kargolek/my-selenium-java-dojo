13c5705ddea50990b97110a2f680e4068ff096f6
karol_orlowicz <karol.orlowicz@gmail.com>
2021-08-16 02:50:52 +0200

    test

--- selenium3/src/test/java/org/myselenium/examples/selenium3/driver/DriverFactory.java
+++ selenium3/src/test/java/org/myselenium/examples/selenium3/driver/DriverFactory.java
@@ -3,6 +3,7 @@ package org.myselenium.examples.selenium3.driver;
 import org.openqa.selenium.WebDriver;
 
 import java.net.MalformedURLException;
+import java.util.Optional;
 
 public class DriverFactory {
 
@@ -24,6 +25,7 @@ public class DriverFactory {
     }
 
     public static WebDriver getDriver() throws MalformedURLException {
-        return System.getenv("CI").equals("true") ? getDriver(DriverType.REMOTE) : getDriver(DriverType.CHROME);
+        return Optional.ofNullable(System.getenv("CI")).orElse("LOCAL").equals("true")
+                ? getDriver(DriverType.REMOTE) : getDriver(DriverType.CHROME);
     }
 }
