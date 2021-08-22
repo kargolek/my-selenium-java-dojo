318496d5eb66beae602d6d968e1113429d0c198e
karol_orlowicz <karol.orlowicz@gmail.com>
2021-08-22 16:59:50 +0200

    fix logger messages for test result types

--- selenium3/src/main/java/org/selenium/examples/selenium3/testWatchers/PrintableTestExecutionResults.java
+++ selenium3/src/main/java/org/selenium/examples/selenium3/testWatchers/PrintableTestExecutionResults.java
@@ -17,14 +17,14 @@ public class PrintableTestExecutionResults implements TestWatcher {
     @Override
     public void testSuccessful(ExtensionContext context) {
         TestWatcher.super.testSuccessful(context);
-        logger.error(String.format("TEST ERROR:\nClassName -> %s\nMethodName -> %s\nDisplayName -> %s",
+        logger.error(String.format("TEST PASSED:\nClassName -> %s\nMethodName -> %s\nDisplayName -> %s",
                 context.getRequiredTestClass().getSimpleName(), context.getRequiredTestMethod().getName(), context.getDisplayName()));
     }
 
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
         TestWatcher.super.testFailed(context, cause);
-        logger.error(String.format("TEST ERROR:\nClassName -> %s\nMethodName -> %s\nDisplayName -> %s\nCause:\n%s",
+        logger.error(String.format("TEST FAILED:\nClassName -> %s\nMethodName -> %s\nDisplayName -> %s\nCause:\n%s",
                 context.getRequiredTestClass().getSimpleName(), context.getRequiredTestMethod().getName(),
                 context.getDisplayName(), cause));
 
